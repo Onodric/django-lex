b7e0d23ce616bd7f7021b68f9b5e35cf576b8cdc
Belve D. Marks <belvedmarks4@gmail.com>
2017-07-08 21:56:15 -0500

    added ERD and plyer model attributes

--- lexicology/crossword/models/player.py
+++ lexicology/crossword/models/player.py
@@ -4,21 +4,29 @@ from django.db import models
 # Create your models here.
 class player(models.Model):
     """
-    A player model with 1-to-1 correlation to a user in the system. To be
-    fleshed out
+    A player model
+    Attributes:
+        user: 1-to-1 correlation to a user in the system.
+        word: many-to-many to a word
+        puzzle: a one-to-many to a dictionary
+        solved_puzzles: an int containing number of solved puzzles
+        solved_words: 
     """
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+
     class Meta:
         """
-
+        Class to set the plural of the model as 'Players'
         """
         verbose_name_plural = "Players"
 
     def __str__(self):
         """
-
+        Method to render a given player as a string. Helpful for
+        interactions.
         :return:
+        A string of the player's username
         """
         return '{}'.format(self.user.username)
